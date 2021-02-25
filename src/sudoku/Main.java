c4393b18d78b0ba08a24e797836c4d99ac45533b
lebrancconvas <lebrancconvas@gmail.com>
2021-02-25 15:47:48 +0700

    :sparkles: Factorial Calculation.

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,15 +1,44 @@
 package sudoku;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements ActionListener {
     JFrame frame;
+    JTextField t1, t2;
+    JButton b1;
     Main() {
-        JButton button = new JButton("Button 1");
-        button.setBounds(100, 100, 100, 100);
-        add(button);
-        setSize(500, 500);
+        t1 = new JTextField();
+        t1.setBounds(100,100,200,50);
+        t2 = new JTextField();
+        t2.setBounds(100, 250, 200, 50);
+        t2.setEditable(false);
+        b1 = new JButton("Calculate");
+        b1.setBounds(100, 400, 100, 50);
+        b1.addActionListener(this);
+        add(t1); add(t2); add(b1);
+        setSize(800, 800);
         setLayout(null);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String s1 = t1.getText();
+        long a = Long.parseLong(s1);
+        long b = 0;
+        if(e.getSource() == b1) {
+            b = factorial(a);
+        }
+        String result = String.valueOf(b);
+        t2.setText(result);
+    }
+
+    public long factorial(long number) {
+        long result = 1;
+        for(long i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
