23d42c49b4209f3a506836832f3dd0ae016fc979
lebrancconvas <lebrancconvas@gmail.com>
2021-02-25 13:43:56 +0700

    :art: Swing by Inheritence.

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,14 +1,18 @@
 package sudoku;
 import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame {
+    JFrame frame;
+    Main() {
+        JButton button = new JButton("Button 1");
+        button.setBounds(100, 100, 100, 100);
+        add(button);
+        setSize(500, 500);
+        setLayout(null);
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("click");
-        button.setBounds(130, 100, 100, 40);
-        frame.add(button);
-        frame.setSize(800, 800);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        new Main();
     }
 }
